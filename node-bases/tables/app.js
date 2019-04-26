0d248c58b4dc60b84f64be1e25639df12149f19d
Alejandro <alejandrehl@icloud.com>
2019-04-26 16:13:45 -0400

    First steps using yargs(argv)

--- node-bases/tables/app.js
+++ node-bases/tables/app.js
@@ -1,15 +1,31 @@
 // const fs = require('fs'); Default
 // const fs = require('express'); //Packages
 // const fs = require('./fs'); //Added to project
+const argv = require('yargs')
+    .command('list', 'Print the multiplication table in the console.', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limit: {
+            alias: 'l',
+            default: 10
+        },
+    })
+    .help()
+    .argv;
 const { createFile } = require('./multiply/multiply'); //Destructuration
 
-let argv = process.argv;
-let param = argv[2];
-let base = param.split("=")[1];
+console.log(argv.base);
+console.log(argv.limit);
 
-createFile(base)
-    .then(file => console.log(`Created file: ${file}`))
-    .catch(err => console.log(`Error: ${err}`));
+// let param = argv[2];
+// let base = param.split("=")[1];
+
+// let base = 5;
+// createFile(base)
+//     .then(file => console.log(`Created file: ${file}`))
+//     .catch(err => console.log(`Error: ${err}`));
 
 // For use the method by console 
 // Exec the folliwing command: node app --base=12
